2c6d44dbe504f52c9374c030893908cfbde57b97
Dhaval <mehtadhaval401@gmail.com>
2018-12-08 22:43:45 +0530

    Reference Variable in CPP

--- reference_variable.cpp
+++ reference_variable.cpp
@@ -4,6 +4,21 @@ int main()
 {
 	int x=10;
 	int &r=x;   //reference to the x as pointer in cpp
-	cout<<"x-"<<x<<endl;
-	cout<<"r-"<<r<<endl;
+	cout<<"x="<<x<<endl;
+	cout<<"r="<<r<<endl;
+	
+	
+	//If reference variable is pointing to perticular variable then it is fixex i.e. constant , 
+	//it can not be referred to another object , error it throws as below
+	//int y=15;
+	//r=y;
+	//cout<<"y-"<<y<<"r-"<<r<<endl;
+
+	//RV for pointer
+	int *p=&x;
+	int *rp=p;
+	cout<<"p="<<*p<<endl;
+	cout<<"rp="<<*rp<<endl;
+	
+
 }
