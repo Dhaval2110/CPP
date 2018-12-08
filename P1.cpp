1135275b59d65ac1a8472b09af71034434c32353
Dhaval <mehtadhaval401@gmail.com>
2018-12-08 22:15:17 +0530

    CPP Basics

--- P1.cpp
+++ P1.cpp
@@ -1,13 +1,27 @@
 #include<iostream>
 using namespace std;
+
+// 1. Basic Syntax
 //int main()
 //{
 //	cout<<"Welcome to C++"<<endl;
 //}
+
+//2. How to scan and print from the user
+//int main()
+//{
+//	int x;
+//	cout<<"enter the value of x";
+//	cin>>x;
+//	cout<<"x-"<<x<<endl;
+//}
+
+//3. Scope resolution operator
+int x=10;
 int main()
 {
-	int x;
-	cout<<"enter the value of x";
-	cin>>x;
-	cout<<"x-"<<x<<endl;
+	int x=5;
+	cout<<"X-"<<x<<endl;
+	cout<<"Scope Resolution, X -"<<::x<<endl;
 }
+
