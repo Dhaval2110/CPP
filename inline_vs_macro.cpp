70fa3f8c7ac745ff418b98cb13a9c306c1600880
Dhaval <mehtadhaval401@gmail.com>
2019-04-06 22:21:53 +0530

    Interview Question

---
+++ inline_vs_macro.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+// using inline function
+//inline int cub(int a)
+//{
+//	return a*a*a;
+//}
+//int main()
+//{
+//	cout<<cub(1+2)<<endl;
+//}
+#def cube(b) b*b*b
+int main()
+{
+	cout<<cube(1+2)<<endl;
+}
